ad9ae9445592b577e088d845144796aa524b5497
canozokur <canozokur@users.noreply.github.com>
2016-08-01 00:19:15 +0300

    Change viewcart order

--- secondarynavbar_viewcart.php
+++ secondarynavbar_viewcart.php
@@ -8,6 +8,6 @@ add_hook('ClientAreaSecondaryNavbar', 1, function (MenuItem $secondaryNavbar)
     $secondaryNavbar->addChild('View Cart', array(
                 'label' => '<i class="fa fa-shopping-cart fa-lg"></i> <sup style="font-size: small"><b>' . $cartCount . '</b></sup>',
                 'uri' => 'cart.php?a=view',
-                'order' => '99',
+                'order' => '98',
             ));
 });
