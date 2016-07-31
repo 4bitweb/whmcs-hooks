12bf98a742c360a5eaf5651be758a926f79576ba
canozokur <canozokur@users.noreply.github.com>
2016-08-01 00:19:33 +0300

    Add language changer to secondary navbar

---
+++ secondarynavbar_language.php
@@ -0,0 +1,29 @@
+<?php
+
+use WHMCS\ClientArea;
+use WHMCS\View\Menu\Item as MenuItem;
+
+add_hook('ClientAreaSecondaryNavbar', 2, function (MenuItem $secondaryNavbar) use ($vars)
+{
+    global $smarty;
+    $vars = $smarty->tpl_vars;
+    $langChange = $vars["languagechangeenabled"]->value;
+    $locales = $vars["locales"]->value;
+    $linkBack = $vars["currentpagelinkback"]->value;
+
+    if ($langChange && count($locales) > 1)
+    {
+        $changeLang = $secondaryNavbar->addChild('change-lang', array(
+            'label' => '<i class="fa fa-globe fa-lg"></i>',
+            'order' => '99',
+        ));
+
+        foreach ($locales as $key => $locale) {
+            $changeLang->addChild($locale["locale"], array(
+                'label' => $locale["localisedName"],
+                'order' => $key,
+                'uri' => $linkBack . 'language=' . $locale["language"],
+            ));
+        }
+    }
+});
