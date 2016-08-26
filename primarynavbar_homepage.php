2db447c6215f8421216a8950198399fbcbcab8a0
canozokur <canozokur@users.noreply.github.com>
2016-08-27 01:47:28 +0300

    Fix primary navbar hook

--- primarynavbar_homepage.php
+++ primarynavbar_homepage.php
@@ -4,8 +4,8 @@ use WHMCS\View\Menu\Item as MenuItem;
  
 add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar)
 {
-    if (!is_null($primaryNavbar->getChild('Primary_Navbar-Home'))) {
-        $primaryNavbar->getChild('Primary_Navbar-Home')
+    if (!is_null($primaryNavbar->getChild('Home'))) {
+        $primaryNavbar->getChild('Home')
             ->addChild('Web Ana Sayfa', array(
                 'label' => Lang::trans('webhomepage'),
                 'uri' => 'https://4-bit.net',
