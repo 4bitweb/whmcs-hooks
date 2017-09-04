30885f04af60f878aff2090748fdff7279963b35
canozokur <canozokur@users.noreply.github.com>
2017-09-04 03:22:42 +0300

    Add jessepolak/card support

---
+++ add_cardjs.php
@@ -0,0 +1,68 @@
+<?php
+
+add_hook('ClientAreaHeadOutput', 50, function($vars) {
+    return <<<HTML
+    <script src="{$vars['BASE_PATH_JS']}/jquery.card.js"></script>
+HTML;
+});
+
+add_hook('ClientAreaFooterOutput', 50, function($vars) {
+    $cardnum = $vars['cardnum'];
+    $cardexp = $vars['cardexp'];
+    $cardtype = $vars['cardtype'];
+
+    $returnVal = <<<HTML
+    <script>
+    var cardjsTypeMap = {
+        'mastercard': 'MasterCard',
+        'visa': 'Visa',
+        'discover': 'Discover',
+        'amex': 'American Express',
+        'jcb': 'JCB',
+        'dinersclub': 'Diners Club'
+    }
+
+    var whmcsTypeMap = {
+        'mastercard': 'mastercard',
+        'visa': 'visa',
+        'discover': 'discover',
+        'american express': 'amex',
+        'jcb': 'jcb',
+        'diners club': 'dinersclub'
+    }
+
+    $('#frmNewCc').card({
+        container: '#credit-card',
+        formSelectors: {
+            numberInput: 'input#inputCardNumber',
+            expiryInput: 'input[name="ccexpirymonth"], input[name="ccexpiryyear"]',
+            cvcInput: 'input[name="cardcvv"]'
+        },
+        masks: {
+            cardNumber: '•'
+        },
+        placeholders: {
+            number: '{$cardnum}',
+            name: '',
+            expiry: '{$cardexp}'
+        }
+    });
+    document.getElementById('inputCardNumber')
+        .addEventListener('payment.cardType', function(event) {
+            $('#inputCardType').val(cardjsTypeMap[event.detail]);
+        });
+    </script>
+HTML;
+
+    if ($cardtype != '')
+    {
+        $cardclass = strtolower($cardtype);
+        $returnVal .= <<<HTML
+        <script>
+        $('.jp-card').removeClass('jp-card-unknown').addClass('jp-card-' + whmcsTypeMap['{$cardclass}']).addClass('jp-card-identified');
+        </script>
+HTML;
+    }
+
+    return $returnVal;
+});
